f27eec366aea87b9fdac788067538a5c4aa45625
Jon Volden <volden.jon@gmail.com>
2019-03-03 19:15:58 -0600

    Added reference

--- formChecker.js
+++ formChecker.js
@@ -47,6 +47,8 @@ function validateForm() {
 
 }
 
+// Code borrowed from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
 function validateEmail(email) {
   let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
